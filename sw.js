900a1f50738c4fae2c43e42fb727e7aefad75562
Aleksandr <a@test.com>
2018-07-08 13:11:25 +0300

    pwa

--- sw.js
+++ sw.js
@@ -1,2 +1,17 @@
 'use strict';
-importScripts('sw-toolbox.js'); toolbox.precache(['index.html','css/main.css']); toolbox.router.get('/img/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+importScripts('sw-toolbox.js'); 
+toolbox.precache(
+    [
+        'index.html',
+        'css/main.css',
+        'css/bootstrap.min.css',
+        'js/bootstrap.min.js',
+        'js/jquery-3.3.1.min.js',
+        'js/popper.min.js',
+    ]
+); 
+toolbox.router.get('/img/*', toolbox.cacheFirst); 
+toolbox.router.get('/*',
+ toolbox.networkFirst, 
+{ networkTimeoutSeconds: 5}
+);
